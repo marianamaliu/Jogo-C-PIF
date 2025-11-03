f7c82cabac880ae0354f696934b7a4c0d15ca680
Mariana <you@example.com>
2025-11-03 18:56:24 -0300

    Inicializando a janela e desenhando o mapa

--- jogo.c
+++ jogo.c
@@ -80,30 +80,69 @@ typedef struct{
     Fantasma *fantasmas; //vetor dinamico p/ ser mais flexivel
     int qntd_fantasmas;
     bool jogo_ativo;
-    int pontuacao;
+    int pontuacao_atual;
 
     Score *pontuacao;
 }Jogo;
 
+void inicalizar(Jogo *Jogo);
+void liberar_mapa(Mapa *m);
+void desenhar_mapa(Jogo *jogo);
+
 int main(){
 
+    Jogo jogo = {0};
+    inicalizar(&jogo);
 
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGH, "Pacman com raylib em C");
+    SetTargetFPS(60);
+
+    while(!WindowShouldClose()){
+
+        BeginDrawing();
+            ClearBackground(BLACK); //fundo preto
+            desenhar_mapa(&jogo);
+        EndDrawing();
+    }
+    liberar_mapa(&jogo.mapa);
+    CloseWindow();
 
 
     return 0;
 }
 
-Mapa *alocar_mapa(int largura, int altura){
-    Mapa *m=(Mapa*)malloc(sizeof(Mapa));
-    
-    m->altura=altura;
-    m->largura=largura;
-    m->mapa=(char**)malloc(altura*sizeof(char*));
+void inicalizar(Jogo *jogo){
+    jogo->mapa.largura = LARGURA;
+    jogo->mapa.altura = ALTURA;
+    jogo->mapa.mapa = (char**)malloc(ALTURA*sizeof(char*)); //alocar a memoria
 
-    for(int i=0; i<altura; i++){
-        m->mapa[i]=(char*)malloc((largura+1)*sizeof(char));
+    for(int i=0; i<ALTURA; i++){
+        jogo->mapa.mapa[i]=(char*)malloc((LARGURA+1)*sizeof(char)); //alocar a memoria
+        strcpy(jogo->mapa.mapa[i], MAPA[i]);
     }
-    return m;
+
+    jogo->jogo_ativo=true;
+    jogo->pontuacao_atual=0;
+
+    jogo->pacman.x=1;
+    jogo->pacman.y=1;
+    jogo->pacman.dx=1;
+    jogo->pacman.dy=0;
+}
+
+void desenhar_mapa(Jogo *jogo){
+    for(int i=0; i<ALTURA; i++){
+        for(int j=0; j<LARGURA; j++){
+            char tile = jogo->mapa.mapa[i][j];
+
+            if(tile=='#'){
+                DrawRectangle(i * TILE_SIZE, j * TILE_SIZE, TILE_SIZE, TILE_SIZE, DARKBLUE);
+
+            }else if(tile == ' '){
+                DrawCircle(i * TILE_SIZE + TILE_SIZE/2, j * TILE_SIZE + TILE_SIZE/2, TILE_SIZE/8, YELLOW);
+            }
+        }
+    }
 }
 
 void liberar_mapa(Mapa *m){
@@ -112,18 +151,4 @@ void liberar_mapa(Mapa *m){
     }
     free(m->mapa);
     m->mapa=NULL;
-}
-
-void inicalizar(Jogo *jogo){
-    jogo->mapa.largura = LARGURA;
-    jogo->mapa.altura = ALTURA;
-    jogo->mapa.mapa = (char**)malloc(ALTURA*sizeof(char*));
-
-    for(int i=0; i<ALTURA; i++){
-        jogo->mapa.mapa[i]=(char*)malloc((LARGURA+1)*sizeof(char));
-        strcpy(jogo->mapa.mapa[i], MAPA[i]);
-    }
-
-    jogo->jogo_ativo=true;
-    jogo->pontuacao=0;
 }
