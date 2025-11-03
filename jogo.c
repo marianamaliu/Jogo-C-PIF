961e78fb089532c250bfd44f6a430a33fdf6f447
Mariana <you@example.com>
2025-11-03 12:10:39 -0300

    Definidindo funções e colocando raylib

--- jogo.c
+++ jogo.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <ncurses.h>
 #include <string.h>
 #include <stdbool.h>
+#include <raylib.h>
 
 #define ALTURA 22
 #define LARGURA 41
+#define TILE_SIZE 20
+#define SCREEN_WIDTH (LARGURA*TILE_SIZE)
+#define SCREEN_HEIGH (ALTURA*TILE_SIZE)
 
 static const char *MAPA[ALTURA] = {
     "########################################",
@@ -48,14 +51,28 @@ typedef struct{
     int x, y; //posicao do fantasma
     int dx, dy; //direcao
     ModoFantasma modo;
-    int cor_ncurses;
 }Fantasma;
 
 typedef struct{
+    int x;
+    int y;
+}Comida;
+
+typedef struct{
+    int x;
+    int y;
+}Fruta;
+
+typedef struct{
     int largura;
-    int altura;
-    char **mapa;
+    int altura; 
+    char **mapa; //mapa[altura][largura]
 }Mapa;
+
+typedef struct Score{
+    int pontos;
+    struct Score *next;
+}Score;
 
 typedef struct{
     Mapa mapa;
@@ -63,8 +80,10 @@ typedef struct{
     Fantasma *fantasmas; //vetor dinamico p/ ser mais flexivel
     int qntd_fantasmas;
     bool jogo_ativo;
-}Jogo;
+    int pontuacao;
 
+    Score *pontuacao;
+}Jogo;
 
 int main(){
 
@@ -72,4 +91,39 @@ int main(){
 
 
     return 0;
+}
+
+Mapa *alocar_mapa(int largura, int altura){
+    Mapa *m=(Mapa*)malloc(sizeof(Mapa));
+    
+    m->altura=altura;
+    m->largura=largura;
+    m->mapa=(char**)malloc(altura*sizeof(char*));
+
+    for(int i=0; i<altura; i++){
+        m->mapa[i]=(char*)malloc((largura+1)*sizeof(char));
+    }
+    return m;
+}
+
+void liberar_mapa(Mapa *m){
+    for(int i=0; i<m->altura; i++){
+        free(m->mapa[i]);
+    }
+    free(m->mapa);
+    m->mapa=NULL;
+}
+
+void inicalizar(Jogo *jogo){
+    jogo->mapa.largura = LARGURA;
+    jogo->mapa.altura = ALTURA;
+    jogo->mapa.mapa = (char**)malloc(ALTURA*sizeof(char*));
+
+    for(int i=0; i<ALTURA; i++){
+        jogo->mapa.mapa[i]=(char*)malloc((LARGURA+1)*sizeof(char));
+        strcpy(jogo->mapa.mapa[i], MAPA[i]);
+    }
+
+    jogo->jogo_ativo=true;
+    jogo->pontuacao=0;
 }
