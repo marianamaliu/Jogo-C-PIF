3cff262777f4cc0ae6db98e2f11726e8303aee76
Mariana <you@example.com>
2025-11-04 11:46:25 -0300

    Desenhando Fruta, pacman pegando fruta e bolinhas pequenas

--- jogo.c
+++ jogo.c
@@ -92,8 +92,10 @@ void inicalizar(Jogo *jogo);
 void liberar_mapa(Mapa *m);
 void desenhar_mapa(Jogo *jogo);
 void desenhar_pacman(Pacman pacman);
-void gerar_comida(Jogo *jogo);
 void desenhar_comida(Comida comida);
+void desenhar_fruta(Fruta fruta);
+void gerar_comida(Jogo *jogo);
+void gerar_fruta(Jogo *jogo);
 void colisao(Jogo *jogo);
 void atualizar_posi(Jogo *jogo);
 
@@ -114,6 +116,7 @@ int main(){
             desenhar_mapa(&jogo);
             desenhar_pacman(jogo.pacman);
             desenhar_comida(jogo.comida);
+            desenhar_fruta(jogo.fruta);
         EndDrawing();
     }
     liberar_mapa(&jogo.mapa);
@@ -142,6 +145,7 @@ void inicalizar(Jogo *jogo){
     jogo->pacman.dy=0;
 
     gerar_comida(jogo);
+    gerar_fruta(jogo);
 }
 
 void desenhar_mapa(Jogo *jogo){
@@ -178,12 +182,34 @@ void desenhar_comida(Comida comida){
 
 }
 
+void desenhar_fruta(Fruta fruta){
+    float pixelX=(float)(fruta.x*TILE_SIZE);
+    float pixelY=(float)(fruta.y*TILE_SIZE);
+
+    int centroX=pixelX+TILE_SIZE/2;
+    int centroY=pixelY+TILE_SIZE/2;
+    int raio_cereja=TILE_SIZE/4;
+
+    //caule 1
+    DrawLine(centroX, centroY-raio_cereja*2, centroX-raio_cereja, centroY+raio_cereja-raio_cereja/2, GREEN);
+    //caule 2
+    DrawLine(centroX, centroY-raio_cereja*2, centroX+raio_cereja, centroY+raio_cereja-raio_cereja/2, GREEN);
+    //cereja 1
+    DrawCircle(centroX-raio_cereja, centroY+raio_cereja, raio_cereja, RED);
+    //cereja 2
+    DrawCircle(centroX+raio_cereja, centroY+raio_cereja, raio_cereja, RED);
+    //brilho da cereja
+    DrawCircle(centroX - raio_cereja - raio_cereja/4, centroY + raio_cereja - raio_cereja/4, raio_cereja/4, WHITE);
+    DrawCircle(centroX + raio_cereja - raio_cereja/4, centroY + raio_cereja - raio_cereja/4, raio_cereja/4, WHITE);
+}
+
 void gerar_comida(Jogo *jogo){
     Comida *comida=&jogo->comida;
     //aleatoriedade
     int novoX = rand() % (LARGURA-2) + 1; 
     int novoY = rand() % (ALTURA-2) + 1;
 
+    //se for na parede, ele sorteia os valores de novo ate nao ser na parede
     if(jogo->mapa.mapa[novoY][novoX]=='#'){
         gerar_comida(jogo);
     }else{
@@ -192,10 +218,28 @@ void gerar_comida(Jogo *jogo){
     }
 }
 
+void gerar_fruta(Jogo *jogo){
+    Fruta *fruta=&jogo->fruta;
+    Comida *comida=&jogo->comida;
+
+    //aleatoriedade
+    int novoX=rand()%(LARGURA-2)+1;
+    int novoY=rand()%(ALTURA-1)+1;
+
+    //se for parede ou se for onde ja tem comida, ele gera de novo ate nao ser parede ou nao ter comida
+    if(jogo->mapa.mapa[novoY][novoX]=='#' || jogo->mapa.mapa[novoY][novoX]==comida->x && jogo->mapa.mapa[novoY][novoX]==comida->y){
+        gerar_fruta(jogo);
+    }else{
+        fruta->x=novoX;
+        fruta->y=novoY;
+    }
+}
+
 void colisao(Jogo *jogo){
     Pacman *pacman=&jogo->pacman;
     Comida *comida=&jogo->comida;
-    
+    Fruta *fruta=&jogo->fruta;
+
     int proximoX = pacman->x + pacman->dx;
     int proximoY = pacman->y + pacman->dy;
 
@@ -204,12 +248,24 @@ void colisao(Jogo *jogo){
     jogo->mapa.mapa[proximoY][proximoX]!='#'){
         pacman->x = proximoX;
         pacman->y = proximoY;
+
+        //detectar bolinhas menores
+        if(jogo->mapa.mapa[proximoY][proximoX]=' '){
+            jogo->mapa.mapa[proximoY][proximoX]='.'; //ele desenha a bolinha, entao quando vai para a função desenhar, o espaço nao esta mais vazio,
+            //entao ele nao desenha a bolinha amarela
+        }
     }
     
     //detectar comida
     if(proximoX==comida->x && proximoY==comida->y){
         jogo->pontuacao_atual+=10;
         gerar_comida(jogo);
+    }
+
+    //detectar fruta
+    if(proximoX==fruta->x && proximoY==fruta->y){
+        jogo->pontuacao_atual+=50;
+        gerar_fruta(jogo);
     }
 
 }
