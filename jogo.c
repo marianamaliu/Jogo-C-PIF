0717a44b383513e7ece43acd3e6fb006e2c34951
Mariana <you@example.com>
2025-11-03 21:29:32 -0300

    Desenhando pacman e add comentarios

--- jogo.c
+++ jogo.c
@@ -7,11 +7,12 @@
 
 #define ALTURA 22
 #define LARGURA 41
-#define TILE_SIZE 20
-#define SCREEN_WIDTH (LARGURA*TILE_SIZE)
-#define SCREEN_HEIGH (ALTURA*TILE_SIZE)
+#define TILE_SIZE 20 //tamanho em pixels de cada bloco na tela
+#define JANELA_LARGURA (LARGURA*TILE_SIZE) //calcula a largura total da janela em pixels
+#define JANELA_ALTURA (ALTURA*TILE_SIZE) //calcula a altura total da janela em pixels
 
-static const char *MAPA[ALTURA] = {
+static const char *MAPA[ALTURA] = { //layout do labirinto; cada elemento do array é um ponteiro p um caractere; 
+    //cada ponteiro aponta para o inicio de uma linha do labirinto
     "########################################",
     "#                   #                  #",
     "#  ####    ######   #   ######   ####  #",
@@ -88,13 +89,14 @@ typedef struct{
 void inicalizar(Jogo *Jogo);
 void liberar_mapa(Mapa *m);
 void desenhar_mapa(Jogo *jogo);
+void desenhar_pacman(Pacman pacman);
 
 int main(){
 
     Jogo jogo = {0};
     inicalizar(&jogo);
 
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGH, "Pacman com raylib em C");
+    InitWindow(JANELA_LARGURA, JANELA_ALTURA, "Pacman com raylib em C");
     SetTargetFPS(60);
 
     while(!WindowShouldClose()){
@@ -102,6 +104,7 @@ int main(){
         BeginDrawing();
             ClearBackground(BLACK); //fundo preto
             desenhar_mapa(&jogo);
+            desenhar_pacman(jogo.pacman);
         EndDrawing();
     }
     liberar_mapa(&jogo.mapa);
@@ -131,18 +134,29 @@ void inicalizar(Jogo *jogo){
 }
 
 void desenhar_mapa(Jogo *jogo){
-    for(int i=0; i<ALTURA; i++){
-        for(int j=0; j<LARGURA; j++){
-            char tile = jogo->mapa.mapa[i][j];
+    for(int i=0; i<ALTURA; i++){ //y
+        for(int j=0; j<LARGURA; j++){ //x
+            char tile = jogo->mapa.mapa[i][j]; //pega a ALTURA e LARGURA do mapa do layout do labirinto
 
             if(tile=='#'){
-                DrawRectangle(i * TILE_SIZE, j * TILE_SIZE, TILE_SIZE, TILE_SIZE, DARKBLUE);
-
+                DrawRectangle( j * TILE_SIZE,  i * TILE_SIZE, TILE_SIZE, TILE_SIZE, DARKBLUE);
+                //pega a posição da linha i/y e multiplica pelo tam do bloco p obter a coordenada i(x) na tela
             }else if(tile == ' '){
-                DrawCircle(i * TILE_SIZE + TILE_SIZE/2, j * TILE_SIZE + TILE_SIZE/2, TILE_SIZE/8, YELLOW);
+                DrawCircle( j * TILE_SIZE + TILE_SIZE/2,   i * TILE_SIZE + TILE_SIZE/2, TILE_SIZE/8, YELLOW);
+                //calcula o centro do bloco para o ponto ficar centralizado em cada bloco; TILE_SIZE/8 define o raio do circulo (20/8=2.5 pixels)
             }
         }
     }
+}
+
+void desenhar_pacman(Pacman pacman){
+    
+    float pixelX = (float)(pacman.x *TILE_SIZE);
+    float pixelY = (float)(pacman.y *TILE_SIZE);
+
+    DrawCircle(pixelX + TILE_SIZE/2, pixelY + TILE_SIZE/2, TILE_SIZE/2-2, YELLOW);
+    //pixelX (onde o bloco comça), TILE_SIZE/2 (centro do bloco na horizontal), pixelX+TILE_SIZE/2 (soma do ponto inicial com o centro, onde o centro do circulo estará)
+    //TILE_SIZE/2-2 (centro do bloco - 2pixels , para definir o tamanhgo do pacman)
 }
 
 void liberar_mapa(Mapa *m){
